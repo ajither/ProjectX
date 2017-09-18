cf567bad8096756978a95731f39cc6481f457060
ajither <ajitheredacheril@hotmail.com>
2017-09-18 19:57:49 +0530

    Login Screen Layout

---
+++ app/src/main/java/com/projectx/projectx/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.projectx.projectx.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projectx.projectx.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
