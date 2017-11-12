cf143cb4817af8c82cd27738cae3f6b574f15de3
ajither <ajitheredacheril@hotmail.com>
2017-11-13 01:23:41 +0530

    Coustomized Navigation Drawer View Completed.

--- app/src/main/java/com/projectx/projectx/MainActivity.java
+++ app/src/main/java/com/projectx/projectx/MainActivity.java
@@ -14,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.projectx.projectx.Config.ProjectXPref;
 import com.projectx.projectx.Fragment.ActionLikeDislike;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity
     private HomeScreen homeScreen;
     private LikeDislikeScreen likeDislikeScreen;
     private DeleteMyAccountScreen deleteMyAccountScreen;
+    private ImageView navbarClose, paxlFaq;
 
     private FragmentTransaction transaction;
     @Override
@@ -50,6 +53,18 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        View header = navigationView.getHeaderView(0);
+        navbarClose = (ImageView) header.findViewById(R.id.close_nav_bar);
+        navbarClose.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            }
+        }
+        });
 
         prefManager = new ProjectXPref(this);
         logoutFragment = new Logout();
@@ -101,18 +116,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_interests) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logout) {
+            checkLikeDislikeSet();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
